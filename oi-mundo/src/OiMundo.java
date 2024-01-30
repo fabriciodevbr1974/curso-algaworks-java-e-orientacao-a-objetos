eaceb63c96ff4d1fccf3d63501115b95c7118b70
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-01-30 19:08:16 -0300

    Inclusão de comentários no código fonte

--- oi-mundo/src/OiMundo.java
+++ oi-mundo/src/OiMundo.java
@@ -2,6 +2,14 @@
 public class OiMundo {
 
 	public static void main(String[] args) {
+		//A linha abaixo envia uma mensagem para o monitor, saída padrão
+		
+		
+		/*
+		 Esta linha será ignorada pelo compilador
+		 System.out.println("Essa instrução será ignorada")
+		 */
+		
 		System.out.println("Oi Mundo");
 	}
 
