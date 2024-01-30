eb75e43fab2504e9d93af9e5ec81522bf3e78733
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-01-30 19:32:29 -0300

    Variáveis do tipo inteiro

---
+++ variaveis/src/Variaveis.java
@@ -0,0 +1,12 @@
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		int quantidade;//Declarando variável inteira
+		quantidade = 10;//atribuindo o valor 10
+		System.out.println(quantidade);
+		quantidade = 15;
+		System.out.println(quantidade);
+	}
+
+}
