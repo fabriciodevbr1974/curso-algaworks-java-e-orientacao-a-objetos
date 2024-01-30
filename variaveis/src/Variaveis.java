3e268fa4a6e6b458a3870f736e23883668769217
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-01-30 19:38:05 -0300

    Criação de variáveis do tipo inteiro

--- variaveis/src/Variaveis.java
+++ variaveis/src/Variaveis.java
@@ -2,11 +2,28 @@
 public class Variaveis {
 
 	public static void main(String[] args) {
-		int quantidade;//Declarando variável inteira
-		quantidade = 10;//atribuindo o valor 10
+		int quantidade;//Declarando variável do tipo inteiro
+		quantidade = 10;//Atribuíndo o valor 10
 		System.out.println(quantidade);
 		quantidade = 15;
 		System.out.println(quantidade);
+		
+		//--Nome de variáveis---------------------------------------
+		int quantidade_alunos;
+		int QUANTIDADE;
+		int QuantidadeAlunoas;
+		int $quantidade;
+		int _quantidade;
+		int quantidade_alunos_nota_10;
+		
+		//Seguido a convensão
+		
+		int quantidadeAlunos;
+		int quantidadeAlunosNota10;
+		int numeroDeAlunosAprovados;
+		
+		
+		
 	}
 
 }
