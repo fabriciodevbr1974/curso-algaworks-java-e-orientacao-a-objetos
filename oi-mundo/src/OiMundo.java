7d853cc82506cdf5687e5fce71a1803591ae23b5
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-01-30 19:18:19 -0300

    Sequencias de escape

--- oi-mundo/src/OiMundo.java
+++ oi-mundo/src/OiMundo.java
@@ -2,15 +2,9 @@
 public class OiMundo {
 
 	public static void main(String[] args) {
-		//A linha abaixo envia uma mensagem para o monitor, saída padrão
-		
-		
-		/*
-		 Esta linha será ignorada pelo compilador
-		 System.out.println("Essa instrução será ignorada")
-		 */
-		
-		System.out.println("Oi Mundo");
+		System.out.println("Oi \"Mundo\"");//Imprime "
+		System.out.println("Oi \\Mundo\\");//Imprime \
+		System.out.println("Oi \nMundo");//Pulou uma linha
 	}
 
 }
