f66d3d8c8bc9c8958b0c29bd803120ef198fcd1f
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-08 00:07:43 -0300

    Outros operadores de atribuição

---
+++ outros-operadores-de-atribuicao/src/Operador2.java
@@ -0,0 +1,29 @@
+
+public class Operador2 {
+
+	public static void main(String[] args) {
+
+		int total = 10;
+		total += 3;
+		System.out.println(total);
+		
+		total -= 2;
+		System.out.println(total);
+		
+		total *= 3;
+		System.out.println(total);
+		
+		total /= 3;
+		System.out.println(total);
+
+		total %= 5;
+		System.out.println(total);
+
+
+		
+		
+		
+		
+	}
+
+}
