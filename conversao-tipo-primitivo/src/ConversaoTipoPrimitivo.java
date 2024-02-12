01c17d9fc84ee3ddf448eea213c36bf3125028ba
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-12 11:40:55 -0300

    Aula 2.13 >> Conversão de tipos primitivos

--- conversao-tipo-primitivo/src/ConversaoTipoPrimitivo.java
+++ conversao-tipo-primitivo/src/ConversaoTipoPrimitivo.java
@@ -3,9 +3,49 @@ public class ConversaoTipoPrimitivo {
 
 	public static void main(String[] args) {
 		
-		long x = 10;
+//		long x = 10;
+//		//casting
+//		int y = (int) x;
+//		System.out.println(y);
+		
+//		long x = 93000000000035L;
+//		int y = (int) x;
+//		System.out.println(y);
+		
+//		int x = 102344;
+//		long y = (int) x;
+//		System.out.println(y);
+
+//		double x = 20.5;
+//		//casting
+//		float y = (float) x;
+//		System.out.println(y);	
+		
+//		float x = 20.5f;
+//		double y = x;
+//		System.out.println(y);
+		
+//		double x = 100;
+//		int y = (int) x;
+//		System.out.println(y);
+		
+		double x = 100.25;
 		int y = (int) x;
 		System.out.println(y);
+		
+		
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		
 
 	}
 
