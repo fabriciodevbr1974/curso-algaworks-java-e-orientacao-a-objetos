48fcec107d5c0fd9be797c9d261a3acbfd4191f9
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-14 11:10:28 -0300

    Aula 2.14 >> Promoção aritmetica

---
+++ PromocaoAritmetica/src/PromocaoAritmetica.java
@@ -0,0 +1,68 @@
+public class PromocaoAritmetica {
+
+  public static void main(String[] args) {
+
+    int x = 3;
+    int y = 2;
+    float z = x / (float)y;
+    System.out.println(z);
+
+
+//    int x = 3;
+//    int y = 2;
+//    float z = (float)x / y;
+//    System.out.println(z);
+
+
+
+//    int x = 3;
+//    float y = 2;
+//    float z = x / y;
+//    System.out.println(z);
+
+
+//    float x = 3;
+//    int y = 2;
+//    float z = x / y;
+//    System.out.println(z);
+
+
+
+
+//    int x = 3;
+//    int y = 2;
+//    float z = x / y;
+//    System.out.println(z);
+
+
+
+//    long x = 10;
+//    float y = 9.34f;
+//    float z = x * y;
+//    System.out.println(z);
+
+
+//    long x = 10;
+//    float y = 9.34f;
+//    long z = x * y;
+//    System.out.println(z);
+
+
+
+//    int x = 10;
+//    long y = 5;
+//    long z = x + y;
+//    System.out.println(z);
+
+//    int x = 10;
+//    long y = 5;
+//    int z = x + y;
+//    System.out.println(z);
+
+//    int x = 10;
+//    int y = 5;
+//    int z = x + y;
+//    System.out.println(z);
+
+  }
+}
