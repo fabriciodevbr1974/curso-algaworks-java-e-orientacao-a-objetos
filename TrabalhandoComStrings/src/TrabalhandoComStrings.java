c11feb9af0715146f71578c069239c4a725f22f7
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-14 21:18:29 -0300

    Aula 2.16 >> Trabalhando com strings

---
+++ TrabalhandoComStrings/src/TrabalhandoComStrings.java
@@ -0,0 +1,24 @@
+public class TrabalhandoComStrings {
+
+  public static void main(String[] args) {
+
+    System.out.println("Monstrando texto na tela");
+
+    int x = 10;
+    int y = 5;
+    int z = x + y;
+    System.out.println("Resultado: " + z);
+    System.out.println("Resultado: " + x + y);//Concatenou
+    System.out.println("Resultado: " + (x + y));//Inteiros
+
+    String nome = "Maria";
+    int idade = 30;
+
+    System.out.println(nome + " tem " + idade + " anos");
+
+
+
+  }
+
+
+}
